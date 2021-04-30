a063cfb413fa9bd5a006e70a87e40b9720ecbd98
Chris Hillenbrand <chillenb.lists@gmail.com>
2021-04-30 19:15:57 -0400

    relative paths

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    publicPath: './',
   },
   plugins: [
     new HtmlWebpackPlugin({
